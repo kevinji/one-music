0f7e1e7e7f5b9fe628de89f565672676d846dbc7
Kevin Ji <kevin.ji@outlook.com>
2014-10-04 16:18:21 -0700

    Add console.log about server running

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ fs.readFile("./static/index.html", function(err, html) {
     if (err) {
         throw err;
     }
+
+	var port = process.env.PORT || 8000;
+
     http.createServer(function(request, response) {
         response.writeHeader(200, {"Content-Type": "text/html"});
         response.write(html);
         response.end();
-    }).listen(process.env.PORT || 8000);
+    }).listen(port);
+
+	console.log("OneMusic server running on port " + port + ".");
 });
