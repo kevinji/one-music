9a8feebb1d24a2c09a7814bded18fa426759b9f8
Kevin Ji <kevin.ji@outlook.com>
2014-10-04 15:51:46 -0700

    Listen on port 80

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ fs.readFile("./index.html", function(err, html) {
         response.writeHeader(200, {"Content-Type": "text/html"});
         response.write(html);
         response.end();
-    }).listen(8000);
+    }).listen(80);
 });
