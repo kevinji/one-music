fe6d4674e5813544b0f07bec78f8e831821dec79
Kevin Ji <kevin.ji@outlook.com>
2014-10-04 20:51:04 -0700

    dropbox: Add get function

--- public/js/dropbox.js
+++ public/js/dropbox.js
@@ -1,9 +1,8 @@
 (function($, undefined) {
-	var Dropbox = {},
-		APP_KEY = "2zjh7j8tn8ajjzf";
+	var Dropbox = {};
 
-	Dropbox.AUTH_URL = "https://www.dropbox.com/1/oauth2/authorize";
-	Dropbox.auth = function() {
+	Dropbox.INITIAL_AUTH_URL = "https://www.dropbox.com/1/oauth2/authorize";
+	Dropbox.initialAuth = function() {
 		var params = {
 				client_id: APP_KEY,
 				response_type: "token",
@@ -11,7 +10,21 @@
 			},
 			param = $.param(params);
 
-		window.location.href = Dropbox.AUTH_URL + "?" + param;
+		window.location.href = Dropbox.INITIAL_AUTH_URL + "?" + param;
+	};
+
+	Dropbox.GET_URL = "https://api-content.dropbox.com/1/files/auto/"
+	Dropbox.get = function(authCode, file, callback) {
+		var url = Dropbox.GET_URL + encodeURIComponent(file);
+
+		$.ajax({
+			url: url,
+			type: "GET",
+			dataType: "text",
+			headers: {
+				"Authorization": "Bearer " + authCode
+			}
+		}).done(callback);
 	};
 
 	// Expose Dropbox to window
