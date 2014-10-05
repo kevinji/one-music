7f115c7e1cfbb02f0af5e5cb51acec43c0b54824
Kevin Ji <kevin.ji@outlook.com>
2014-10-04 17:43:23 -0700

    Include jQuery

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,7 @@
-(function(undefined) {
+(function($, undefined) {
 	var firebase = new Firebase("https://onemusic.firebaseio.com/");
-}());
+
+	$("#js-upload-button").on("submit", function() {
+		// Upload file to Dropbox
+	});
+}(jQuery));
